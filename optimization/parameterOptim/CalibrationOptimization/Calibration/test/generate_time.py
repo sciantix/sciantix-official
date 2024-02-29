8c457aed8fb3c66fa9684bf455c43432b68e61a0
ShuoWang2000 <wshuo0078@gmail.com>
2024-02-29 20:55:41 +0100

    add cali_test

--- optimization/parameterOptim/CalibrationOptimization/Calibration/test/generate_time.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tools
-import subprocess
-
-subprocess.run(['./sciantix.x'])
-
-time_model = tools.get_selected_variables_value_from_output(["Time (h)"], 'output.txt')
-np.savetxt('time_model.txt', time_model)
