84d108e4e51a91716ae7fbff89bb0e286e682ac1
kasimaru13 <kasimaru13@gmail.com>
2024-07-11 13:23:22 +0200

    main reconstruct

--- src/mainSCIANTIX.C
+++ src/mainSCIANTIX.C
@@ -36,12 +36,98 @@
  *
  */
 
+#include "MainVariables.h"
+#include "Sciantix.h"
+#include "InputInterpolation.h"
+#include "InputReading.h"
+#include "Initialization.h"
+#include "TimeStepCalculation.h"
+#include "Bounds.h"
+#include "Global.h"
+#include "ErrorMessages.h"
 #include <iostream>
+#include <fstream>
+#include <ctime>
 
 using namespace std;
 
+
+
+/**
+ * \brief Logs the execution time of the simulation.
+ * @param timer The total execution time measured in seconds.
+ * @param time_step_number The total number of time steps executed during the simulation.
+ */
+void logExecutionTime(double timer, int time_step_number);
+
+
+
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        std::cerr << "Warning: No path specified, using current directory." << std::endl;
+        std::cout << "Try to use ./sciantix.x ../regression/your-test-file-path/ in the bin directory" << std::endl;
+        TestPath = "./";
+    } else {
+        TestPath = argv[1];
+        if (TestPath.back() != '/') {
+            TestPath += '/';
+        }
+    }
 
-    return 0;
+
+	InputReading();
+
+    Initialization();
+
+	std::string outputPath = TestPath + "output.txt";
+
+	remove(outputPath.c_str());
+
+	Execution_file.open(TestPath + "execution.txt", std::ios::out);
+
+	timer = clock();
+
+
+    while (Time_h <= Time_end_h)
+    {
+        Sciantix_history[0] = Sciantix_history[1];
+		Sciantix_history[1] = InputInterpolation(Time_h, Time_input, Temperature_input, Input_history_points);
+		Sciantix_history[2] = Sciantix_history[3];
+		Sciantix_history[3] = InputInterpolation(Time_h, Time_input, Fissionrate_input, Input_history_points);
+		if (Sciantix_history[3] < 0.0)
+			Sciantix_history[3] = 0.0;
+		Sciantix_history[4] = Sciantix_history[5];
+		Sciantix_history[5] = InputInterpolation(Time_h, Time_input, Hydrostaticstress_input, Input_history_points);
+		Sciantix_history[7] = Time_h;
+		Sciantix_history[8] = static_cast<double>(Time_step_number);
+		Sciantix_history[9] = Sciantix_history[10];
+		Sciantix_history[10] = InputInterpolation(Time_h, Time_input, Steampressure_input, Input_history_points);
+
+
+        // TO DO SCIANTIX
+
+
+        dTime_h = TimeStepCalculation();
+		Sciantix_history[6] = dTime_h * 3600;
+
+		if (Time_h < Time_end_h)
+		{
+			Time_step_number++;
+			Time_h += dTime_h;
+			Time_s += Sciantix_history[6];
+		}
+		else
+			break;
+    }
+
+
+    timer = clock() - timer;
+
+
+	logExecutionTime((double)timer / CLOCKS_PER_SEC, Time_step_number);
+	Execution_file.close();
+
+
+	return 0;
 }
