ae2aae77543f44b08ce3c2e74a621e6bf37f41b8
kasimaru13 <kasimaru13@gmail.com>
2024-07-19 15:29:49 +0200

    setGas

---
+++ include/utilities/setters/SetGas.h
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//       _______.  ______  __       ___      .__   __. .___________. __  ___   ___  //
+//      /       | /      ||  |     /   \     |  \ |  | |           ||  | \  \ /  /  //
+//     |   (----`|  ,----'|  |    /  ^  \    |   \|  | `---|  |----`|  |  \  V  /   //
+//      \   \    |  |     |  |   /  /_\  \   |  . `  |     |  |     |  |   >   <    //
+//  .----)   |   |  `----.|  |  /  _____  \  |  |\   |     |  |     |  |  /  .  \   //
+//  |_______/     \______||__| /__/     \__\ |__| \__|     |__|     |__| /__/ \__\  //
+//                                                                                  //
+//  Originally developed by D. Pizzocri & T. Barani                                 //
+//                                                                                  //
+//  Version: 2.0                                                                    //
+//  Year: 2022                                                                      //
+//  Authors: D. Pizzocri, G. Zullo.                                                 //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SET_GAS_H
+#define SET_GAS_H
+
+#include <vector>
+#include "Gas.h"
+#include "SciantixArray.h"
+
+/**
+ * \brief Sets up the gas properties in the simulation.
+ * 
+ * This function initializes and maps properties for Xenon, Krypton, and Helium gases
+ * used in the simulation. Each gas is set up with specific attributes and then mapped
+ * for easy access throughout the simulation.
+ */
+void setGas(SciantixArray<Gas> &gas);
+
+void xenon(SciantixArray<Gas> &gas);
+void krypton(SciantixArray<Gas> &gas);
+void helium(SciantixArray<Gas> &gas);
+
+#endif // SET_GAS_H
