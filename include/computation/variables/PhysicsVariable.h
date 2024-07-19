b1e9bc7622da7b19baa695e77d6b948f0abb51f7
AaZz <theaazz@outlook.com>
2024-07-19 13:33:19 +0200

    Remove code in the .h to put it in .C

--- include/computation/variables/PhysicsVariable.h
+++ include/computation/variables/PhysicsVariable.h
@@ -49,128 +49,83 @@ public:
 	 * @brief Rescales the initial value by a specified factor.
 	 * @param factor The multiplier to apply to the initial value.
 	 */
-	void rescaleInitialValue(const double factor)
-	{
-		// Function to rescale the final value
-		initial_value *= factor;
-	}
+	void rescaleInitialValue(const double factor);
 
 	/**
 	 * @brief Rescales the final value by a specified factor.
 	 * @param factor The multiplier to apply to the final value.
 	 */
-	void rescaleFinalValue(const double factor)
-	{
-		// Function to rescale the final value
-		final_value *= factor;
-	}
+	void rescaleFinalValue(const double factor);
 
 	/**
 	 * @brief Adds a specified value to the final value.
 	 * @param v The value to add to the final value.
 	 */
-	void addValue(const double v)
-	{
-		// Function to increase final_value by v
-		final_value += v;
-	}
+	void addValue(const double v);
 
 	/**
 	 * @brief Sets the unit of measure for this variable.
 	 * @param s The string representing the unit of measure.
 	 */
-	void setUOM(std::string s)
-	{
-		uom = s;
-	}
+	void setUOM(std::string s);
 
 	/**
 	 * @brief Retrieves the unit of measure.
 	 * @return The unit of measure as a string.
 	 */
-	std::string getUOM()
-	{
-		return uom;
-	}
+	std::string getUOM();
 
 	/**
 	 * @brief Sets the final value equal to the initial value, making the variable constant over time.
 	 */
-	void setConstant()
-	{
-		final_value = initial_value;
-	}
+	void setConstant();
 
 	/**
 	 * @brief Resets the initial value to match the final value, synchronizing them.
 	 */
-	void resetValue()
-	{
-		initial_value = final_value;
-	}
+	void resetValue();
 
 	/**
 	 * @brief Sets the final value to a specified value.
 	 * @param FinalValue The value to set as the final value.
 	 */
-	void setFinalValue(double FinalValue)
-	{
-		final_value = FinalValue;
-	}
+	void setFinalValue(double FinalValue);
 
 	/**
 	 * @brief Sets the initial value to a specified value.
 	 * @param InitialValue The value to set as the initial value.
 	 */
-	void setInitialValue(double InitialValue)
-	{
-		initial_value = InitialValue;
-	}
+	void setInitialValue(double InitialValue);
 
 	/**
 	 * @brief Retrieves the final value of the variable.
 	 * @return The final value.
 	 */
-	double getFinalValue()
-	{
-		return final_value;
-	}
+	double getFinalValue();
 
 	/**
 	 * @brief Retrieves the initial value of the variable.
 	 * @return The initial value.
 	 */
-	double getInitialValue()
-	{
-		return initial_value;
-	}
+	double getInitialValue();
 
 	/**
 	 * @brief Calculates and returns the increment between the final and initial values.
 	 * @return The calculated increment.
 	 */
-	double getIncrement()
-	{
-		return final_value - initial_value;
-	}
+	double getIncrement();
 
 	/**
 	 * @brief Sets whether the variable should be included in output.
 	 * @param io A boolean value indicating output inclusion.
 	 */
-	void setOutput(bool io)
-	{
-		to_output = io;
-	}
+	void setOutput(bool io);
 
 	/**
 	 * @brief Checks if the variable is marked for output.
 	 * @return True if the variable is to be included in output, false otherwise.
 	 */
-	bool getOutput()
-	{
-		return to_output;
-	}
+	bool getOutput();
 
 	/**
 	 * @brief Default constructor for PhysicsVariable.
---
+++ include/material/system/System.h
@@ -0,0 +1,244 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//       _______.  ______  __       ___      .__   __. .___________. __  ___   ___  //
+//      /       | /      ||  |     /   \     |  \ |  | |           ||  | \  \ /  /  //
+//     |   (----`|  ,----'|  |    /  ^  \    |   \|  | `---|  |----`|  |  \  V  /   //
+//      \   \    |  |     |  |   /  /_\  \   |  . `  |     |  |     |  |   >   <    //
+//  .----)   |   |  `----.|  |  /  _____  \  |  |\   |     |  |     |  |  /  .  \   //
+//  |_______/     \______||__| /__/     \__\ |__| \__|     |__|     |__| /__/ \__\  //
+//                                                                                  //
+//  Originally developed by D. Pizzocri & T. Barani                                 //
+//                                                                                  //
+//  Version: 2.0                                                                    //
+//  Year: 2022                                                                      //
+//  Authors: D. Pizzocri, G. Zullo.                                                 //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include "Matrix.h"
+#include "Gas.h"
+#include "InputVariableDeclaration.h"
+#include "MapInputVariable.h"
+#include "HistoryVariableDeclaration.h"
+#include "MapHistoryVariable.h"
+#include "ErrorMessages.h"
+#include <cmath>
+
+/**
+ * \class System
+ * \brief Class derived from Gas and Matrix to include the properties that depend on both the fission gas and the fuel matrix (e.g., Xe covolume)
+ */
+class System : virtual public Gas, virtual public Matrix
+{
+protected:
+	double yield;
+	double radius_in_lattice;
+	double volume_in_lattice;
+	double diffusivity;
+	double bubble_diffusivity;
+	double henry_constant;
+	double resolution_rate;
+	double trapping_rate;
+	double nucleation_rate;
+	std::string gas_name;
+	std::string matrix_name;
+	double pore_nucleation_rate;
+	std::vector<double> modes;
+	double production_rate;
+	bool restructured_matrix;
+
+public:
+	/**
+	 * @brief Sets the matrix restructuring status.
+	 * @param y True if the matrix is restructured, false otherwise.
+	 */
+	void setRestructuredMatrix(bool y);
+
+	/**
+	 * @brief Gets the restructuring status of the matrix.
+	 * @return True if the matrix is restructured, false otherwise.
+	 */
+	bool getRestructuredMatrix();
+
+	/**
+	 * @brief Sets the cumulative yield of the fission gas.
+	 * @param y Yield to set (atoms per fission).
+	 */
+	void setYield(double y);
+
+	/**
+	 * @brief Gets the cumulative yield of the fission gas.
+	 * @return Yield of the gas (atoms per fission).
+	 */
+	double getYield();
+
+	/**
+	 * @brief Sets the radius of the fission gas atom in the matrix lattice.
+	 * @param r Radius to set (meters).
+	 */
+	void setRadiusInLattice(double r);
+	/**
+	 * @brief Gets the radius of the fission gas atom in the matrix lattice.
+	 * @return Radius of the gas atom (meters).
+	 */
+	double getRadiusInLattice();
+
+	/**
+	 * @brief Sets the name of the gas.
+	 * @param n Name of the gas to set.
+	 */
+	void setGasName(std::string n);
+
+	/**
+	 * @brief Gets the name of the gas.
+	 * @return Name of the gas.
+	 */
+	std::string getGasName();
+	/**
+	 * @brief Sets the name of the matrix.
+	 * @param n Name of the matrix to set.
+	 */
+	void setMatrixName(std::string n);
+
+	/**
+	 * @brief Gets the name of the matrix.
+	 * @return Name of the matrix.
+	 */
+	std::string getMatrixName();
+
+	/**
+	 * @brief Gets the volume occupied by the gas in the matrix.
+	 * @return Volume occupied by the gas.
+	 */
+	double getVolumeInLattice();
+	/**
+	 * @brief Sets the volume occupied by the gas in the matrix.
+	 * @param v Volume to set.
+	 */
+	void setVolumeInLattice(double v);
+
+	/**
+	 * @brief Sets the diffusivity of bubbles within the matrix based on input values.
+	 * @param input_value The model selection index for bubble diffusivity.
+	 */
+	void setBubbleDiffusivity(int input_value);
+
+	/**
+	 * @brief Retrieves the diffusivity of bubbles within the matrix.
+	 * @return The current bubble diffusivity.
+	 */
+	double getBubbleDiffusivity();
+
+	/**
+	 * @brief Sets the helium diffusivity within the matrix based on the selected model.
+	 * The intra-granular helium diffusivity within the fuel grain is set according to the input_variable iHeDiffusivity
+	 * @param input_value The model selection index for helium diffusivity.
+	 */
+	void setHeliumDiffusivity(int input_value);
+
+	/**
+	 * @brief Retrieves the helium diffusivity within the matrix.
+	 * @return The current helium diffusivity.
+	 */
+	double getHeliumDiffusivity();
+
+	/**
+	 * @brief Sets the diffusivity of fission gases within the matrix based on the selected model.
+	 * The intra-granular fission gas (xenon and krypton) diffusivity within the fuel grain is set according to the input_variable iFGDiffusionCoefficient
+	 * @param input_value The model selection index for fission gas diffusivity.
+	 */
+	void setFissionGasDiffusivity(int input_value);
+
+	/**
+	 * @brief Retrieves the diffusivity of fission gases within the matrix.
+	 * @return The current fission gas diffusivity.
+	 */
+	double getFissionGasDiffusivity();
+
+	/**
+	 * @brief Sets the Henry's constant value.
+	 * @param h The new value for Henry's constant.
+	 */
+	void setHenryConstant(double h);
+
+	/**
+	 * @brief Retrieves the Henry's constant value.
+	 * @return The current value of Henry's constant.
+	 */
+	double getHenryConstant();
+
+	/**
+	 * @brief Sets the resolution rate for isotopes from nanobubbles in the matrix.
+	 * The helium intra-granular resolution rate is set according to the input_variable iResolutionRate.
+	 * @param input_value The model selection index for setting the resolution rate.
+	 */
+	void setResolutionRate(int input_value);
+
+	/**
+	 * @brief Retrieves the resolution rate for isotopes from nanobubbles in the matrix.
+	 * @return The current resolution rate.
+	 */
+	double getResolutionRate();
+
+	/**
+	 * @brief Sets the trapping rate for isotopes in nanobubbles within the matrix.
+	 * The krypton intra-granular trapping rate is set according to the input_variable iTrappingRate.
+	 * @param input_value The model selection index for setting the trapping rate.
+	 */
+	void setTrappingRate(int input_value);
+
+	/**
+	 * @brief Retrieves the trapping rate for isotopes in nanobubbles within the matrix.
+	 * @return The current trapping rate.
+	 */
+	double getTrappingRate();
+
+	/**
+	 * @brief Sets the nucleation rate based on the selected model.
+	 * Evaluation of the nucleation rate of intragranular gas bubble inside the UO<sub>2</sub> matrix
+	 * @param input_value The model selection index for nucleation rate.
+	 */
+	void setNucleationRate(int input_value);
+
+	/**
+	 * @brief Retrieves the nucleation rate.
+	 * @return The current nucleation rate.
+	 */
+	double getNucleationRate();
+
+	/**
+	 * @brief Sets the pore nucleation rate.
+	 * @param t The new pore nucleation rate.
+	 */
+	void setPoreNucleationRate(double t);
+
+	/**
+	 * @brief Retrieves the pore nucleation rate.
+	 * @return The current pore nucleation rate.
+	 */
+	double getPoreNucleationRate();
+
+	/**
+	 * @brief Sets the production rate based on the selected model.
+	 * @param input_value The model selection index for setting the production rate.
+	 */
+	void setProductionRate(int input_value);
+
+	/**
+	 * @brief  Member function to get the production rate of the sciantix_system.
+	 * @return The current production rate.
+	 */
+	double getProductionRate();
+	/**
+	 * @brief Default constructor for the System class.
+	 */
+	System() {}
+	/**
+	 * @brief Destructor for the System class
+	 */
+	~System() {}
+};
+
+#endif // SYSTEM_H
