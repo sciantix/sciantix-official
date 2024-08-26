4e89e5827eb979c9e169fa2722d3ce4cf246026b
giozu <giovanni.zullo@polimi.it>
2024-08-26 18:43:11 +0200

    header update

--- src/models/EffectiveBurnup.C
+++ src/models/EffectiveBurnup.C
@@ -9,7 +9,7 @@
 //  Originally developed by D. Pizzocri & T. Barani                                 //
 //                                                                                  //
 //  Version: 2.1                                                                    //
-//  Year: 2023                                                                      //
+//  Year: 2024                                                                      //
 //  Authors: D. Pizzocri, G. Zullo                                                  //
 //                                                                                  //
 //////////////////////////////////////////////////////////////////////////////////////
