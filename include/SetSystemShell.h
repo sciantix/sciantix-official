70ac5057de942928a872eab3daf1de08314d656d
germinaleFarina <luca6.farina@mail.polimi.it>
2024-09-02 23:39:01 +0200

    Keep working on the shell class by adding the other auxiliary classes

---
+++ include/SetSystemShell.h
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//       _______.  ______  __       ___      .__   __. .___________. __  ___   ___  //
+//      /       | /      ||  |     /   \     |  \ |  | |           ||  | \  \ /  /  //
+//     |   (----`|  ,----'|  |    /  ^  \    |   \|  | `---|  |----`|  |  \  V  /   //
+//      \   \    |  |     |  |   /  /_\  \   |  . `  |     |  |     |  |   >   <    //
+//  .----)   |   |  `----.|  |  /  _____  \  |  |\   |     |  |     |  |  /  .  \   //
+//  |_______/     \______||__| /__/     \__\ |__| \__|     |__|     |__| /__/ \__\  //
+//                                                                                  //
+//  Originally developed by D. Pizzocri & T. Barani                                 //
+//                                                                                  //
+//  Version: 2.0                                                                    //
+//  Year: 2022                                                                      //
+//  Authors: D. Pizzocri, G. Zullo.                                                 //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+/// SetSystem
+
+#include "MapSystem.h"
+#include "SystemDeclaration.h"
+#include "MapPhysicsVariable.h"
+#include "PhysicsVariableDeclaration.h"
+#include "GasDeclaration.h"
+#include "MapGas.h"
+#include "Xe_in_SiC.h"
+#include "Kr_in_SiC.h"
+#include "He_in_SiC.h"
+
+#include <vector>
+
+void SetSystemShell();
---
+++ src/SetSystemShell.C
@@ -0,0 +1,105 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//       _______.  ______  __       ___      .__   __. .___________. __  ___   ___  //
+//      /       | /      ||  |     /   \     |  \ |  | |           ||  | \  \ /  /  //
+//     |   (----`|  ,----'|  |    /  ^  \    |   \|  | `---|  |----`|  |  \  V  /   //
+//      \   \    |  |     |  |   /  /_\  \   |  . `  |     |  |     |  |   >   <    //
+//  .----)   |   |  `----.|  |  /  _____  \  |  |\   |     |  |     |  |  /  .  \   //
+//  |_______/     \______||__| /__/     \__\ |__| \__|     |__|     |__| /__/ \__\  //
+//                                                                                  //
+//  Originally developed by D. Pizzocri & T. Barani                                 //
+//                                                                                  //
+//  Version: 2.0                                                                    //
+//  Year: 2022                                                                      //
+//  Authors: D. Pizzocri, G. Zullo.                                                 //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include "SetSystemShell.h"
+
+/// SetSystem
+
+void SetSystemShell()
+{
+			Xe_in_SiC();
+			MapSystem();
+
+			Kr_in_SiC();
+			MapSystem();
+
+			He_in_SiC();
+			MapSystem();
+}
+
+void System::setFissionGasDiffusivity(int input_value)
+{
+	/** 
+	 * ### setFissionGasDiffusivity
+	 * @brief The intra-granular fission gas (xenon and krypton) diffusivity within the fuel grain is set according to the input_variable iFGDiffusionCoefficient
+	 * 
+	 */
+	const double boltzmann_constant = CONSTANT_NUMBERS_H::PhysicsConstants::boltzmann_constant;
+
+	switch (input_value)
+	{
+	case 0:
+	{
+		/**
+		 * @brief iFGDiffusionCoefficient = 0 corresponds to a constant intra-granular diffusivity value, equal to 7e-19 m^2/s.
+		 * 
+		 */
+		
+		reference += "iFGDiffusionCoefficient: constant diffusivity.\n\t";
+		diffusivity = 7e-19;
+		diffusivity *= sf_diffusivity;
+
+		break;
+	}
+
+	case 1:
+	{
+		//@brief iFGDiffusionCoefficient = 1 set the fission gas diffusivity according to "???" STILL TO BE REPLACED 
+
+		break;
+	}
+
+	default:
+		ErrorMessages::Switch(__FILE__, "iFGDiffusionCoefficient", input_value);
+		break;
+	}
+}
+
+void System::setHeliumDiffusivity(int input_value)
+{
+
+	/** 
+	 * ### setHeliumDiffusivity
+	 * @brief The intra-granular helium diffusivity within the fuel grain is set according to the input_variable iHeDiffusivity
+	 * 
+	 */
+	switch (input_value)
+	{
+	case 0:
+	{
+		/**
+		 * @brief iHeDiffusivity = 0 corresponds to a constant intra-granular diffusivity value
+		 * 
+		 */
+		
+		reference += "iHeDiffusivity: constant intragranular diffusivity.\n\t";
+		diffusivity = 7e-19;
+		break;
+	}
+
+	case 1:
+	{
+		/**
+		 * @brief set the diffussivity according to "???" STILL OT BE REPLACED */
+		
+		break;
+	}
+
+	default:
+		ErrorMessages::Switch(__FILE__, "iHeDiffusivity", input_value);
+		break;
+	}
+}
