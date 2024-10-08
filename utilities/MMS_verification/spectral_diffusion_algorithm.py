74bafe31b0ec5f6bfd1ee6224813809a14ccb1b9
ayazayat29 <aya.zayat@mail.polimi.it>
2024-10-08 15:43:07 +0200

    SDA_Python

--- utilities/MMS_verification/spectral_diffusion_algorithm.py
+++ utilities/MMS_verification/spectral_diffusion_algorithm.py
@@ -18,25 +18,16 @@ def SpectralDiffusion1equation(gas_1, initial_condition_gas_1, parameter, increm
 
 	n = 0
 	np1 = 1
-
 	diffusion_rate1 = 0.0
-
 	diffusion_rate_coeff1 = 0.0
-
 	source_rate_coeff_1 = 0.0
-
 	source_rate1 = 0.0
-
 	projection_coeff = 0.0
-
 	gas_1_solution = 0.0
-
 	pi = math.pi
-
+	
 	diffusion_rate_coeff1 = pi**2 * parameter[1] / parameter[2]**2 # pi^2 * D1 / a^2
-
 	projection_coeff = -2.0 * math.sqrt(2.0 / pi)
-
 	source_rate_coeff_1 = projection_coeff * parameter[3] # - 2 sqrt(2/pi) * S1
 
 	# dx_k/dt = -(D*pi^2*k^2/a^2)x_k + <S_k|psi>
@@ -53,31 +44,17 @@ def SpectralDiffusion1equation(gas_1, initial_condition_gas_1, parameter, increm
 
 		diffusion_rate1 = diffusion_rate_coeff1 * np1**2 # pi^2 * D1 * n^2 / a^2
 		source_rate1 = source_rate_coeff_1 * n_coeff # [-  sqrt(8/pi) * S1 * (-1)^n/n] this is S_k
-
 		coeff = 1.0 + (diffusion_rate1) * increment # denomenator when solving the backward euler
-
+        
+		#x(i+1) = (x(i) + <S_k|psi>Dt)/(1+(D*pi^2*k^2/a^2)Dt) | This is the equation below
 		initial_conditions = (initial_condition_gas_1[n] + source_rate1 * increment)/(coeff)
-
-		#Laplace2x2(coeff, initial_conditions) # (A,b)
-
 		initial_condition_gas_1[n] = initial_conditions
-
 		gas_1_solution += projection_coeff * n_coeff * initial_conditions / ((4.0 / 3.0) * pi) #Volume average
 
 	gas_1[0] = gas_1_solution
 
 
-
-# def Laplace2x2(A, b):
-# 	detA = A[0] * A[3] - A[1] * A[2]
-
-# 	if detA != 0.0:
-# 		detX = b[0] * A[3] - b[1] * A[1]
-# 		detY = b[1] * A[0] - b[0] * A[2]
-# 		b[0] = detX / detA
-# 		b[1] = detY / detA
-
-# def main():
+ #def main():
 
 num_steps = 1000
 increment = 0.01
@@ -90,7 +67,7 @@ D1 = 3.0
 a = 2.0
 S1 = 4.0
 
-parameter = [n_modes, D1, a, S1] #We dont need b and g nor teh decay L
+parameter = [n_modes, D1, a, S1] #We dont need b and g nor the decay L
 
 initial_condition_gas_1 = np.zeros(parameter[0])
 
@@ -106,8 +83,8 @@ plt.axhline(y=c_eq1, linestyle='--', label='Equilibrium Concentration 1')
 plt.xlabel('Time')
 plt.ylabel('Concentration')
 plt.legend()
-plt.show()
-plt.savefig('output.png')  # Saves the plot as a file
+plt.grid()
+plt.savefig('output.png')
 # if __name__ == "__main__":
 # 	main()
 
