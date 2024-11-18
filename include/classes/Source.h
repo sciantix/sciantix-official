2c1d9b3503bbebf1588bf84258e059cdc4644795
ayazayat29 <aya.zayat@mail.polimi.it>
2024-11-18 12:53:59 +0100

    Source_Class.Version1

---
+++ include/classes/Source.h
@@ -0,0 +1,105 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//       _______.  ______  __       ___      .__   __. .___________. __  ___   ___  //
+//      /       | /      ||  |     /   \     |  \ |  | |           ||  | \  \ /  /  //
+//     |   (----`|  ,----'|  |    /  ^  \    |   \|  | `---|  |----`|  |  \  V  /   //
+//      \   \    |  |     |  |   /  /_\  \   |  . `  |     |  |     |  |   >   <    //
+//  .----)   |   |  `----.|  |  /  _____  \  |  |\   |     |  |     |  |  /  .  \   //
+//  |_______/     \______||__| /__/     \__\ |__| \__|     |__|     |__| /__/ \__\  //
+//                                                                                  //
+//  Originally developed by D. Pizzocri & T. Barani                                 //
+//                                                                                  //
+//  Version: 2.1                                                                    //
+//  Year: 2024                                                                      //
+//  Authors: D. Pizzocri, G. Zullo.                                                 //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+/**
+ * @class Source
+ * @brief Contains all information about the general source
+ *
+ * 
+ * @author A. Zayat
+ */
+class Source
+{
+public:
+    std::vector<double> NormalizedDomain; // NormalizedDomain = r/a [edge1,edge2,edge3]
+    std::vector<double> Slopes; // [A1, A2, A3]
+    std::vector<double> Intercepts; // [B1, B2, B3]
+
+
+    /**
+     * @brief Sets the normalized domain.
+     * @param rho The normalized domain is set [-].
+     */
+    void setNormalizedDomain(std::vector<double> domain)
+    {
+        NormalizedDomain = domain;
+    }
+
+    /**
+     * @brief Retrieves the domain.
+     * @return The normalized domain of the source [-].
+     */
+    double getNormalizedDomain()
+    {
+        std::vector<double> NormalizedDomain;
+    }
+
+    /**
+     * @brief Sets the slopes of the source in each region.
+     * @param A The slopes to set in each region [atm/m4].
+     */
+    void setLatticeParameter(std::vector<double> A)
+    {
+        Slopes = A;
+    }
+
+    /**
+     * @brief Retrieves the slopes of the source in each region.
+     * @return The slopes of the source in each region [atm/m4].
+     */
+    double getSlopes()
+    {
+        std::vector<double> Slopes;
+    }
+
+    /**
+     * @brief Sets the intercepts of the source in each region.
+     * @param B The intercepts of the source in each region [at/m3]
+     */
+    void setIntercepts(std::vector<double> B)
+    {
+        Intercepts = B;
+    }
+
+    /**
+     * @brief Retrieves the intercepts of the source in each region
+     * @return The intercepts of the source in each region [at/m3]
+     */
+    double getIntercepts()
+    {
+        std::vector<double> Intercepts;
+    }
+
+    /**
+     * @brief Constructor
+     */
+    Source() {}
+
+    /**
+     * @brief Destructor
+     */
+    ~Source() {}
+};
+
+#endif // SOURCE_H
