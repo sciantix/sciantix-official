e5898fc6a3416252c5333c1b1851604ca02fc2c7
ayazayat29 <aya.zayat@mail.polimi.it>
2024-11-19 14:36:24 +0100

    GS_update

--- include/classes/Source.h
+++ include/classes/Source.h
@@ -32,23 +32,23 @@
 class Source
 {
 public:
-    std::vector<double> NormalizedDomain; // NormalizedDomain = r/a [edge1/a,edge2/a,edge3/a,...]
-    std::vector<double> Slopes; // [A1, A2, A3, ...]
-    std::vector<double> Intercepts; // [B1, B2, B3,...]
+    std::array<double,4> NormalizedDomain; // NormalizedDomain = r/a [0,edge1/a,edge2/a,edge3/a]
+    std::array<double,3> Slopes; // [A1, A2, A3]
+    std::array<double,3> Intercepts; // [B1, B2, B3]
 
 
     /**
-     * @brief Sets the normalized domain.
-     * @param rho The normalized domain is set [-].
+     * @brief Sets the normalized domain from [0,1]
+     * @param rho The normalized domain is set [-]
      */
-    void setNormalizedDomain(std::vector<double> domain)
+    void setNormalizedDomain(std::array<double,4> rho)
     {
-        NormalizedDomain = domain;
+        NormalizedDomain = rho;
     }
 
     /**
-     * @brief Retrieves the domain.
-     * @return The normalized domain of the source [-].
+     * @brief Retrieves the domain [0,1]
+     * @return The normalized domain of the source [-]
      */
     double getNormalizedDomain()
     {
@@ -56,28 +56,28 @@ public:
     }
 
     /**
-     * @brief Sets the slopes of the source in each region.
-     * @param A The slopes to set in each region [atm/m4].
+     * @brief Sets the slopes of the source in each region
+     * @param A The slopes to set in each region [atm/m4]
      */
-    void setLatticeParameter(std::vector<double> A)
+    void setLatticeParameter(std::array<double,3> A)
     {
         Slopes = A;
     }
 
     /**
-     * @brief Retrieves the slopes of the source in each region.
-     * @return The slopes of the source in each region [atm/m4].
+     * @brief Retrieves the slopes of the source in each region
+     * @return The slopes of the source in each region [atm/m4]
      */
     double getSlopes()
     {
-        std::vector<double> Slopes;
+        double Slopes;
     }
 
     /**
-     * @brief Sets the intercepts of the source in each region.
+     * @brief Sets the intercepts of the source in each region
      * @param B The intercepts of the source in each region [at/m3]
      */
-    void setIntercepts(std::vector<double> B)
+    void setIntercepts(std::array<double,3> B)
     {
         Intercepts = B;
     }
@@ -88,7 +88,7 @@ public:
      */
     double getIntercepts()
     {
-        std::vector<double> Intercepts;
+        double Intercepts;
     }
 
     /**
