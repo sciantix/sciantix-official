1dfa0f86c011c8164f97e2b6d7a90eb6b1d5855f
ElisaCappellariEC <elisa.cappellari@polimi.it>
2025-04-22 14:31:00 +0200

    uo2 vap2 + experimental data

--- regression/test_Colle2005/thermochemistry.py
+++ regression/test_Colle2005/thermochemistry.py
@@ -1,9 +1,9 @@
 import subprocess
 import shutil
+import os 
 
 # Path to input settings file
 input_file = "input_settings.txt"
-output_file = "output.txt"
 results_dir = "results"
 
 # Function to modify input settings
@@ -20,7 +20,8 @@ def modify_input_settings(value):
 # Function to run sciantix and save output
 def run_sciantix(output_name):
     subprocess.run("./sciantix.x", shell=True, check=True)
-    shutil.move(output_file, f"{results_dir}/{output_name}")
+    shutil.move("output.txt", f"{results_dir}/{output_name}")
+    shutil.move("input_check.txt", f"{results_dir}/input_check_{output_name}")
 
 shutil.copy("../../build/sciantix.x", ".")
 
@@ -29,8 +30,19 @@ modify_input_settings("0")
 run_sciantix("output_nochemistry.txt")
 
 # Run with thermochemistry
-modify_input_settings("3")
+modify_input_settings("2")
 run_sciantix("output_chemistry.txt")
 
 # Run thermochemistry script
 subprocess.run("python3 plot_thermochemistry.py", shell=True, check=True)
+
+folder_name = input("Folder name: ").strip()
+
+if folder_name:
+    os.makedirs(folder_name, exist_ok=True)
+    for file in os.listdir(results_dir):
+        if file.endswith(".png") or file.endswith(".txt"):
+            shutil.move(os.path.join(results_dir, file), os.path.join(folder_name, file))
+    print(f"Results in '{folder_name}'.")
+else:
+    print("Results in the 'results' directory.")
