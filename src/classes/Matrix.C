80441efc562ac549ab8ad4784e3697a49003adb3
UlisseFrattini <ulisse.frattini@gmail.com>
2025-04-24 16:29:09 +0200

    up 24/04

--- src/classes/Matrix.C
+++ src/classes/Matrix.C
@@ -89,9 +89,12 @@ void Matrix::setGrainBoundaryVacancyDiffusivity(int input_value, SciantixArray<S
 
         case 3:
         {
-            // grain_boundary_vacancy_diffusivity = 8.86e-6 * exp(- 5.75e-19 / ( boltzmann_constant * history_variable["Temperature"].getFinalValue())) + 1e-39 * history_variable["Fission rate"].getFinalValue();
+           //grain_boundary_vacancy_diffusivity = 8.86e-6 * exp(- 5.75e-19 / ( boltzmann_constant * history_variable["Temperature"].getFinalValue())) + 1e-39 * history_variable["Fission rate"].getFinalValue();
     
-            grain_boundary_vacancy_diffusivity = 8.86e-6 * exp(- 5.75e-19 / ( boltzmann_constant * history_variable["Temperature"].getFinalValue())) + 5e-41 * history_variable["Fission rate"].getFinalValue();
+           //grain_boundary_vacancy_diffusivity = 8.86e-6 * exp(- 5.75e-19 / ( boltzmann_constant * history_variable["Temperature"].getFinalValue())) + 5e-41 * history_variable["Fission rate"].getFinalValue();
+
+           //Jernkvist vacanzy diffusivity
+           grain_boundary_vacancy_diffusivity = 6.9e-4 * exp(-38770 / history_variable["Temperature"].getFinalValue()) + 5.64e-25 * sqrt(history_variable["Fission rate"].getFinalValue()) * exp(-13800 / history_variable["Temperature"].getFinalValue()) + 1e-39 * history_variable["Fission rate"].getFinalValue();
             
             reference += "iGrainBoundaryVacancyDiffusivity: from White, JNM, 325 (2004), 61-77 / BARANI HBS part 2, 2022\n\t";
 
@@ -100,7 +103,7 @@ void Matrix::setGrainBoundaryVacancyDiffusivity(int input_value, SciantixArray<S
             
             // std::cout << hbs_correction << std::endl;
 
-            grain_boundary_vacancy_diffusivity *= hbs_correction;
+            //grain_boundary_vacancy_diffusivity *= hbs_correction;
 
             break;
         }
@@ -165,7 +168,7 @@ void Matrix::setPoreNucleationRate(SciantixArray<SciantixVariable> &sciantix_var
 
 void Matrix::setPoreResolutionRate(SciantixArray<SciantixVariable> &sciantix_variable, SciantixArray<SciantixVariable> &history_variable, SciantixArray<InputVariable> &scaling_factors)
 {
-    double correction_coefficient = (1.0 - exp(pow(-sciantix_variable["HBS pore radius"].getFinalValue() / (9e-9), 3)));
+    double correction_coefficient = (1.0 - exp(pow(-sciantix_variable["HBS pore radius"].getFinalValue() / (20e-9), 3)));
     double b0(2.0e-23 * history_variable["Fission rate"].getFinalValue());
 
     pore_resolution_rate =
