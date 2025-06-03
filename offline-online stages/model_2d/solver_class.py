b7ca9bd07c40d6068b1b2009b0dc0f9659427aec
martidig <martina.di.gennaro0@gmail.com>
2025-06-03 15:23:21 +0200

    #files

--- offline-online stages/model_2d/solver_class.py
+++ offline-online stages/model_2d/solver_class.py
@@ -5,12 +5,13 @@
 
 # Import libraries:
 import numpy as np
+import scipy as sp 
 import scipy.sparse as sparse
 import scipy.sparse.linalg as linalg
 
 
 # ========================================= #
-#      Forward Solver Full Order Class      #
+#      Backward Solver Full Order Class     #
 # ========================================= #
 
 class FiniteElementCylinderSolver(object):
@@ -61,6 +62,7 @@ class FiniteElementCylinderSolver(object):
         # Assemble left-hand-side and right-hand side (static part):
         self.LHS_static = self.XX_FO + dt * self.SS_FO
         self.RHS_static = dt * self.force
+        self.SPLU = linalg.splu(self.LHS_static)
 
         # Save time delta:
         self.dt = dt
@@ -71,7 +73,8 @@ class FiniteElementCylinderSolver(object):
         # Iteratively solve the problem:
         solution = np.zeros((self.sFO, n_steps+1))
         for ii in range(n_steps):
-            solution[:, ii+1], _ = linalg.bicgstab(self.LHS_static, self.RHS_static + self.XX_FO.dot(solution[: , ii:ii+1]))
+            #solution[:, ii+1], _ = linalg.bicgstab(self.LHS_static, self.RHS_static + self.XX_FO.dot(solution[: , ii:ii+1]))
+            solution[:, ii+1] = self.SPLU.solve(self.RHS_static[:, 0] + self.XX_FO.dot(solution[: , ii]))
 
             # Print current iteration and average temperature and concentration:
             if verbose:
@@ -95,7 +98,7 @@ class FiniteElementCylinderSolver(object):
 
 
 # ============================================ #
-#      Forward Solver Reduced Basis Class      #
+#      Backward Solver Reduced Basis Class     #
 # ============================================ #
 
 class ReducedBasisCylinderSolver(object):
@@ -106,7 +109,7 @@ class ReducedBasisCylinderSolver(object):
     KK_FO = np.load('model_files/KK_FO.npy', allow_pickle=True)
     FF_FO = np.load('model_files/FF_FO.npy')
     AA_FO = np.load('model_files/AA_FO.npy')
-
+ 
     # Load the interpolation matrix, the interpolation coordinates, and the vecotr of coercivity constants:
     II_FO = np.load('model_files/II_FO.npy')
     ZZ_FO = np.load('model_files/ZZ_FO.npy')
@@ -118,11 +121,11 @@ class ReducedBasisCylinderSolver(object):
     
     # Define the solver class clonstructor:
     def __init__(self, basis):
-
+ 
         # Save basis functions and reduced basis space size:
         self.basis = basis
         self.sRB = basis.shape[1]
-
+ 
         # Reduce Y mass matrix:
         self.YY_RB = basis.T @ self.YY_FO @ basis
         self.AA_RB = basis.T @ self.AA_FO
@@ -130,34 +133,34 @@ class ReducedBasisCylinderSolver(object):
         # Assemble Rietz representer and reduced basis projection of the forcing term:
         self.FF_rietz = linalg.spsolve(self.YY_FO, self.FF_FO)
         self.FF_RB    = basis.T @ self.FF_FO
-
+ 
         # Assemble Rietz representer and reduced basis projection of the mass matrix:
         self.XX_right = self.XX_FO @ basis
-        self.XX_rietz = linalg.spsolve(self.YY_FO, self.XX_right)
+        self.XX_rietz = linalg.spsolve(self.YY_FO, self.XX_right).reshape(basis.shape)
         self.XX_RB    = basis.T @ self.XX_right
-
+ 
         # Assemble Rietz representer and reduced basis projection of the partial stiffness matrix:
         self.KK_right = np.array([self.KK_FO[ii] @ basis for ii in range(self.sHR)])
-        self.KK_rietz = np.array([linalg.spsolve(self.YY_FO, self.KK_right[ii]) for ii in range(self.sHR)])
+        self.KK_rietz = np.array([linalg.spsolve(self.YY_FO, self.KK_right[ii]).reshape(basis.shape) for ii in range(self.sHR)])
         self.KK_RB    = np.array([basis.T @ self.KK_right[ii] for ii in range(self.sHR)])
-
+ 
         # Pre-assemble the scalar and vector terms for the residual energy computation:
         self.TT_pr_bb = self.FF_rietz.T @ self.FF_FO
         self.TT_pr_bm = self.FF_rietz.T @ self.XX_right 
         self.TT_pr_mm = self.XX_rietz.T @ self.XX_right
-
+ 
         # Pre-assemble all the tensor terms for the residual energy computation:
         self.TT_pr_ab = np.array( [self.KK_right[ii, :, :].T @ self.FF_rietz for ii in range(self.sHR)])
         self.TT_pr_am = np.array( [self.KK_right[ii, :, :].T @ self.XX_rietz for ii in range(self.sHR)])
         self.TT_pr_aa = np.array([[self.KK_right[ii, :, :].T @ self.KK_rietz[jj, :, :] for ii in range(self.sHR)] for jj in range(self.sHR)])
-    
+        
     # Define method assembling left-hand-side and right-hand-side:
     def assemble(self, RADIUS=1E-5, LENGTH=1E-3, F_RATE=3E+19, F_YIELD=0.24, F_ENERGY = 3.215e-11, T_BC=2E+03, C_BC=0.0, KTH = 2.208, dt=1E+4):
-
+ 
         # Define reference sources intensity:
         self.sgroup_T = (F_RATE * F_ENERGY * LENGTH**2)/(KTH) 
-        self.source_C = F_RATE * F_YIELD 
-
+        self.source_C = F_RATE * F_YIELD
+ 
         # Define heat and mass diffusion coefficiets:
         self.alpha_C = 7.60E-10 * np.exp(- 4.86E-19 / (T_BC + self.sgroup_T * (1 - self.ZZ_FO**2) / 2) / 1.380649E-23) +\
                        5.64E-25 * np.exp(- 1.91E-19 / (T_BC + self.sgroup_T * (1 - self.ZZ_FO**2) / 2) / 1.380649E-23) * np.sqrt(F_RATE) +\
@@ -167,63 +170,64 @@ class ReducedBasisCylinderSolver(object):
         self.inter = np.hstack((self.II_FO @ self.alpha_C / RADIUS**2, self.II_FO @ self.alpha_C / LENGTH**2))
         self.coerc = self.inter @ self.CC_FO
         self.force = self.source_C * self.FF_RB
-
+ 
         # Assemble the scalar and vector terms for the residual energy computation:
         self.DD_pr_bb = self.TT_pr_bb * (self.source_C * self.source_C)
         self.DD_pr_bm = self.TT_pr_bm * (- 2 * self.source_C / dt)
         self.DD_pr_mm = self.TT_pr_mm * (  1 / (dt * dt))
-
+ 
         # Assemble matrices for the computation of the residual energy for the current parameter configuration:
         self.DD_pr_ab = np.tensordot(self.inter, self.TT_pr_ab, axes=[0, 0]) * (-2 * self.source_C)
         self.DD_pr_am = np.tensordot(self.inter, self.TT_pr_am, axes=[0, 0]) * ( 2 / dt)
         self.DD_pr_aa = np.tensordot(self.inter, np.tensordot(self.inter, self.TT_pr_aa, axes=[0, 0]), axes=[0, 0])
-
+ 
         # Assemble reduced basis stiffnes matrix for this parameter configuration:
         self.SS_RB = np.tensordot(self.inter, self.KK_RB, axes=[0, 0])
         
         # Assemble left-hand-side and right-hand side (static part):
         self.LHS_static = self.XX_RB + dt * self.SS_RB
         self.RHS_static = dt * self.force
-
+        self.LU, self.PIV = sp.linalg.lu_factor(self.LHS_static)
+ 
         # Save time delta:
         self.dt = dt
-
+ 
     #  Define reduced basis solver to solve the problem over a given number of iterations:
     def solve(self, n_steps=1000, verbose=False, error_bound=True):
-
+ 
         # Iteratively solve the problem:
         solution = np.zeros((self.sRB, n_steps+1))
         for ii in range(n_steps):
-            solution[:, ii+1] = np.linalg.solve(self.LHS_static, self.RHS_static + self.XX_RB.dot(solution[: , ii:ii+1]))[:, 0]
-
+            solution[:, ii+1] = sp.linalg.lu_solve((self.LU, self.PIV), self.RHS_static[:, 0] + self.XX_RB.dot(solution[: , ii]))
+ 
             # Print current iteration and average temperature and concentration:
             if verbose:
                 print('Current time:', (ii+1) * self.dt, 's')
                 print('Average concentration:', self.AA_RB @ solution[:, ii+1], 'atm/m^3\n')
-
+ 
         # Return solution and in case the error bound:
         if error_bound: return solution, np.sqrt(self.residual_energy(solution) / self.solution_energy(solution) / self.coerc)
         return solution
-
+ 
     # Define a method to compute the squared norm of a solution:
     def solution_energy(self, solution):
         return self.dt * np.einsum('ik,ik', self.SS_RB @ solution, solution) + self.XX_RB.dot(solution[:, -1]).dot(solution[:, -1])
-
+ 
     # Define a method to compute the squared norm of the residual of a solution:
     def residual_energy(self, solution):
-
+ 
         # Compute solution increments:
         increment = solution[:, 1:] - solution[:, :-1]
-
+ 
         # Rreturn squared residual norm:
         return self.dt * (np.einsum('ik,ik', self.DD_pr_am @ increment, solution[:, 1:])       + np.sum(self.DD_pr_bm @ increment) +\
                           np.einsum('ik,ik', self.DD_pr_aa @ solution[:, 1:], solution[:, 1:]) + np.sum(self.DD_pr_ab @ solution[:, 1:]) +\
                           np.einsum('ik,ik', self.DD_pr_mm @ increment, increment)             + self.DD_pr_bb[0] * increment.shape[1] )
-
+ 
     # Get the full order representation of the reduced basis state:
     def get_FO(self, solution):
         return self.basis @ solution
-
+ 
     # Get the reduced basis representation of the full order state:
     def get_RB(self, solution):
         return np.linalg.solve(self.XX_RB, self.XX_right.T @ solution)
