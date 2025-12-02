78a36aada50cbe1af630e1e70d524faa3209d02c
giozu <giovanni.zullo@polimi.it>
2025-12-02 17:05:59 +0100

    update regression.py

--- regression/regression.py
+++ regression/regression.py
@@ -16,11 +16,12 @@ from regression_white import regression_white
 from regression_talip import regression_talip
 from regression_contact import regression_contact
 from regression_oxidation import regression_oxidation
-from regression_kashibe import regression_kashibe
+from regression_kashibe1993 import regression_kashibe1993
+from regression_kashibe1990 import regression_kashibe1990
+from regression_kashibe1991 import regression_kashibe1991
 from regression_hbs import regression_hbs
 from regression_chromium import regression_chromium
 from regression_cornell import regression_cornell
-from regression_baker_GPR import regression_baker_GPR
 
 def remove_output(file):
     os.chdir(file)
@@ -61,15 +62,16 @@ def main():
 
     regression_modules = [
         ('Baker', regression_baker),
-        ('Kashibe', regression_kashibe),
+        ('Kashibe 1990', regression_kashibe1990),
+        ('Kashibe 1991', regression_kashibe1991),
+        ('Kashibe 1993', regression_kashibe1993),
         ('White', regression_white),
         ('Talip', regression_talip),
         ('CONTACT', regression_contact),
         ('oxidation', regression_oxidation),
         ('HBS', regression_hbs),
         ('Chromium', regression_chromium),
-        ('Cornell', regression_cornell),
-        ('GPR', regression_baker_GPR)
+        ('Cornell', regression_cornell)
     ]
 
     # Check if running in GitHub Actions environment
@@ -93,7 +95,7 @@ def main():
 
         # Option 2: Remove all output files
         if execution_option == 2:
-            directories = ["Baker", "Kashibe", "White", "Talip", "CONTACT", "oxidation", "HBS", "Chromium", "Cornell", "GPR"]
+            directories = ["Baker", "Kashibe 1990", "Kashibe 1991", "Kashibe 1993", "White", "Talip", "CONTACT", "oxidation", "HBS", "Chromium", "Cornell"]
             for file in os.listdir(wpath):
                 if any(dir_name in file for dir_name in directories) and os.path.isdir(file):
                     remove_output(file)
@@ -106,7 +108,7 @@ def main():
             # Run all regression tests with default modes
             for name, func in regression_modules:
                 folderList_part, tests_count, tests_failed_count = func(
-                    wpath, 1, mode_gold, mode_plot, [], 0, 0)
+                    wpath, 1, mode_gold, mode_plot, [], 0, 0) [0:3]
                 folderList += folderList_part
                 total_tests += tests_count
                 total_tests_failed += tests_failed_count
@@ -116,14 +118,17 @@ def main():
             print("Possible regression options \n")
             for i, (name, _) in enumerate(regression_modules):
                 print(f"{name} : {i}")
-            regression_mode = int(input("Enter the chosen regression (0, 1, 2, 3, 4, 5, 6, 7, 8, 9) = "))
+            regression_mode = int(input("Enter the chosen regression (0, 1, 2, 3, 4, 5, 6, 7, 8) = "))
 
             mode_gold, mode_plot = get_mode_selections()
 
             # Run the selected regression test
             mode_name, regression_func = regression_modules[regression_mode]
-            folderList, tests_count, tests_failed_count = regression_func(
+            regression_file = regression_func(
                 wpath, 1, mode_gold, mode_plot, [], 0, 0)
+            folderList = regression_file[0]
+            tests_count = regression_file[1]
+            tests_failed_count = regression_file[2]
             total_tests += tests_count
             total_tests_failed += tests_failed_count
             print(f"\nRegression selected: {mode_name}")
