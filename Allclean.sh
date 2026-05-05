bb72b83032e3d3bd63dc114e60b0ccf76a55ca52
giozu <giovanni.zullo@polimi.it>
2026-05-05 21:34:30 +0200

    clean: removed Zone.Identifier files

--- Allclean.sh
+++ Allclean.sh
@@ -5,6 +5,10 @@ set -e
 cd "${0%/*}" || exit 1
 
 echo "== CLEANING BUILD AND PYTHON CACHE =="
+
+# Cleaning all Zone.Identifier files
+find . -type f -name '*:Zone.Identifier' -exec rm -f {} \;
+
 rm -rf build
 rm -rf obj
 
