d3b3b33776a9673cd4330109d84dd4c05c126804
ElisaCappellariEC <elisa.cappellari@polimi.it>
2026-06-27 17:31:33 +0200

    documentation

---
+++ regression/white/__init__.py
@@ -0,0 +1,6 @@
+"""### NEO4MAT - SCIANTIX-DIVA: White regression metadata utilities.
+
+This package contains the White case-study helpers used to generate
+machine-readable JSON/JSON-LD files without changing the original
+SCIANTIX regression inputs and outputs workflow.
+"""
